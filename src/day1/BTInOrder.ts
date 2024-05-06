df0b96d80f57c7f8a132a6a9db1469d138eaee41
yasser110306 <yasserrabie110306@gmail.com>
2024-05-06 16:48:23 +0300

    Refactor binary tree traversal functions

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,20 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]) {
+    // base case
+    if (!curr) {
+        return path;
+    }
+    
+    // recurse
+    walk(curr.left, path);
+    // in
+    path.push(curr.value);
+    //recurse
+    walk(curr.right, path);
 
+
+    // post
+    return path;
+}
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
